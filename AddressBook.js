56fd3e2ff73f88fc6c8298af523edbf4c2bc0c20
“teji1998” <tejasvinirpk@gmail.com>
2020-12-23 19:25:10 +0530

    Added:Code to view contact by city or state

--- AddressBook.js
+++ AddressBook.js
@@ -172,3 +172,16 @@ SearchCityOrState('Mumbai');
 //search for state
 console.log("Searching with state");
 SearchCityOrState('Karnataka');
+
+//UC9:View contact by city or state
+function ViewCityOrState(cityOrState){
+    if(contacts.filter((p=>p.city==cityOrState)||(p=>p.state==cityOrState))){
+        console.log(contacts.toString());
+    }
+}
+//View By city
+console.log("searching with city");
+ViewCityOrState('Hubli');
+//View By state
+console.log("searching with state");
+ViewCityOrState('Maharashtra');
